e2d43d4b20a04308846b0f517305eef23a740b92
zhangmingwei <zjjzmw1@163.com>
2016-07-13 10:22:40 +0800

    添加几个类型的swift文件

--- swiftDemo1/TypeTest.playground/Contents.swift
+++ swiftDemo1/TypeTest.playground/Contents.swift
@@ -1,8 +1,13 @@
 //: Playground - noun: a place where people can play
 /*
- 1：值类型 ----------- 元组(Tuple) 、结构体(Struct) 、枚举(Enum)  基本类型：整数(Int)、浮点数(Float)、布尔值(Bool)、字符串(String)、数组(Array)和字典(Dictionary) 。
+ 1：值类型 ----------- :
+    元组(Tuple)
+    结构体(Struct)
+    枚举(Enum)
+    基本类型：整数(Int)、浮点数(Float)、布尔值(Bool)、字符串(String)、数组(Array)和字典(Dictionary)
  
- 2：引用类型 --------- 类(Class)
+ 2：引用类型 --------- :
+    类(Class)
  
  
  
